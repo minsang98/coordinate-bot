a030a2907f71f638982e630320ada1a94e99523c
minsang98 <blood89@nate.com>
2023-03-29 06:33:46 +0900

    대소문자 구분 x, 로그인 상태 유지 추가

--- index.js
+++ index.js
@@ -29,38 +29,38 @@ client.on("messageCreate", async (message) => {
   const commandBody = message.content.slice(prefix.length);
   const args = commandBody.split(" ");
   const command = args.shift().toLowerCase();
-
-  if (command === "좌표") {
-    if (
-      args.length !== 8 ||
-      !["W", "E"].includes(args[0]) ||
-      !["S", "N"].includes(args[4])
-    ) {
-      return message.reply("좌표 형식이 올바르지 않습니다.");
-    }
-    if (!event) {
-      event = true;
-      const replyMsg = await message.reply(
-        `좌표 ${args.join(" ")}를 검색합니다.`
-      );
-
-      fs.access("coordinate.png", fs.constants.F_OK, (err) => {
-        if (err) return;
-
-        // 파일 삭제
-        fs.unlink("coordinate.png", (err) => {
-          if (err) throw err;
-        });
-      });
-
-      await coordinate(args);
-      await replyMsg.delete({ timeout: 100 });
-      await message.reply({ files: [{ attachment: "./coordinate.png" }] });
-      event = false;
-    } else {
-      return message.reply("기다려");
-    }
+  console.log(args);
+  // if (command === "좌표") {
+  if (
+    args.length !== 6 ||
+    !["W", "E"].includes(args[0].toUpperCase()) ||
+    !["S", "N"].includes(args[3].toUpperCase())
+  ) {
+    return message.reply("좌표 형식이 올바르지 않습니다.");
   }
+  if (!event) {
+    event = true;
+    const replyMsg = await message.reply(
+      `좌표 ${args.join(" ")}를 검색합니다.`
+    );
+
+    fs.access("coordinate.png", fs.constants.F_OK, (err) => {
+      if (err) return;
+
+      // 파일 삭제
+      fs.unlink("coordinate.png", (err) => {
+        if (err) throw err;
+      });
+    });
+
+    await coordinate(args);
+    await replyMsg.delete({ timeout: 100 });
+    await message.reply({ files: [{ attachment: "./coordinate.png" }] });
+    event = false;
+  } else {
+    return message.reply("기다려");
+  }
+  // }
 });
 
 client.login(process.env.TOKEN);
